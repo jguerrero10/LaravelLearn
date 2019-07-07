6a65f3a131622f43fa69788fe99ca1c503ced15b
VS Code Joel <joelgur10@gmail.com>
2019-07-06 22:40:16 -0500

    Cambios menores

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Boxed Layout</title>
+        <title>@yield('titulo', 'Biblioteca')</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -40,22 +40,7 @@
             <div class="content-wrapper">
                     <!-- Content Header (Page header) -->
                 <section class="content">
-                    <div class="box">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Title</h3>
-                        
-                            <div class="box-tools pull-right">
-                                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                                    <i class="fa fa-minus"></i>
-                                </button>                                
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            Start creating your amazing application!
-                        </div>
-                                <!-- /.box-body -->
-                       
-                    </div>
+                    
                 </section>
             </div> 
             <!-- Inicia Footer -->
