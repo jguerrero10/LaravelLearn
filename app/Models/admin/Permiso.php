bc2d45da193b02746376af883b43bce065fe2a12
VS Code Joel <joelgur10@gmail.com>
2019-07-07 21:40:32 -0500

    Contenido

---
+++ app/Models/admin/Permiso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permiso extends Model
+{
+    protected $table = "permiso";
+}
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@
 */
 
 Route::get('/', 'InicioController@index');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('permiso', 'PermisoController@index')->name('permiso');
+    Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
+}); 
